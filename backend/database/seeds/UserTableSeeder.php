f2cd0defe77aafc8809c2ac5d67de0208c0bf6bd
ss-kitagawa <derasado04@gmail.com>
2021-07-08 23:46:53 +0900

    seederファイルの追加

---
+++ backend/database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'テスト',
+            'shop_id' => '1',
+            'position_id' => '1',
+            'email' => 'test@gmail.com',
+            'email_verified_at' => date('Y-m-d H:i:s'),
+            'password' => Hash::make('12345678'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
